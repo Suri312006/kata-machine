a309b63c23270af82d08fa4b6ef555737b50445c
suri312006 <suri312006@gmail.com>
2023-12-25 21:19:49 -0800

    some other stuff

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    value: T,
+    prev?: Node<T>
+}
 export default class Stack<T> {
     public length: number;
-
+    private head?: Node<T>
     
 
     constructor() {
+        this.length = 0
+        this.head = undefined
     }
 
+
     push(item: T): void {
+        this.length ++;
+
+        const newNode = {
+            value: item,
+        } as Node<T>
+        
+        if(!this.head){
+            this.head = newNode
+            return;
+        }
+        newNode.prev = this.head
+        this.head = newNode
 
 }
     pop(): T | undefined {
+        this.length = Math.max(0, this.length-1)
+        if (this.length === 0){
+            const head = this.head
+            this.head = undefined
+            return head?.value;
+        }
+        const prev_node = this.head
+        this.head = this.head?.prev
+        return prev_node?.value
 
 }
     peek(): T | undefined {
+        return this.head?.value
 
 }
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,49 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    //jump by sqrt of n
+    const jump_amt = Math.floor(Math.sqrt(breaks.length))
+    let breakpoint = 0; //* not sure if this is intended behavior
+
+    let i = jump_amt;
+
+    for(; i<breaks.length; i+=jump_amt){
+        const broken = breaks[i]
+
+        if( broken){
+            breakpoint = i
+            break
+        }
+    }
+
+    for(let j = breakpoint - jump_amt; j <= breakpoint; ++j){
+        let value = breaks[j]
+        if(value){
+            return j
+        }
+    }
+    return -1
 }
+//* primes solution underneath
+/* export default function two_crystal_balls(breaks: boolean[]): number {
+
+    //jump by sqrt of n
+    const jump_amt = Math.floor(Math.sqrt(breaks.length))
+
+    let i = jump_amt;
+
+    for(; i<breaks.length; i+=jump_amt){
+        const broken = breaks[i]
+
+        if( broken){
+            break
+        }
+    }
+i -= jump_amt
+    for(let j = 0; j < jump_amt && i<breaks.length; ++j, ++i){
+        let value = breaks[i]
+        if ( value ){
+            return  i
+        }
+    }
+    return -1
+} */
