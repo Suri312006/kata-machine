cfe95976b767e0f1f3d0c4452c28bbf11499240f
Surendra <suri312006@gmail.com>
2023-12-31 20:11:08 -0800

    implemented tree traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,23 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
 
+function walk(curr: BinaryNode<number> | null, path: number[]): number[]{
+    //base case when curr is undefined
+    if (!curr){
+        return path;
+    }
+
+    //pre
+
+    //recurse
+    walk(curr.left, path);
+    path.push(curr.value)
+    walk(curr.right, path)
+
+    //post
+    // we dont do anything in post
+    return path
+
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+return walk(head, [])
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,23 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
 
+return walk(head, [])
+}
+
+function walk(curr: BinaryNode<number> | null, path: number[]): number[]{
+    //base case when curr is undefined
+    if (!curr){
+        return path;
+    }
+
+    //pre
+
+    path.push(curr.value)
+    //recurse
+    walk(curr.left, path);
+    walk(curr.right, path)
+
+    //post
+    // we dont do anything in post
+    return path
+
 }
